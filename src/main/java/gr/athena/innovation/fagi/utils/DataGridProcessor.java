60d7d3cc00ff499907cb8940d55eacf20c6cc415
nkaragiannakis <karagiannakis.nk@gmail.com>
2017-06-09 18:14:24 +0300

    basic implementation for file to grid spliting

---
+++ src/main/java/gr/athena/innovation/fagi/utils/DataGridProcessor.java
@@ -0,0 +1,69 @@
+package gr.athena.innovation.fagi.utils;
+
+import com.vividsolutions.jts.index.strtree.STRtree;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author nkarag
+ */
+public class DataGridProcessor {
+
+    private final String filepath;
+
+    public DataGridProcessor(String filepath){
+        this.filepath = filepath;
+    }
+
+    public void splitDataset(){
+        //todo
+        //use jts spatial index:
+        //split files at any line and for each file build on the same spatial index  
+        if(canLoadToMemory(filepath)){
+            STRtree tree = new STRtree();
+            
+        }
+    }
+
+    private int estimateGridCount() throws FileNotFoundException, IOException{
+        
+        FileInputStream inputStream = null;
+        Scanner sc = null;
+
+        try {
+            inputStream = new FileInputStream(filepath);
+            sc = new Scanner(inputStream, "UTF-8");
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                // System.out.println(line);
+            }
+            //Scanner suppresses exceptions
+            if (sc.ioException() != null) {
+                throw sc.ioException();
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (sc != null) {
+                sc.close();
+            }
+        }
+        return 1;
+    }
+
+    private boolean canLoadToMemory(String filepath) {
+        
+        File file = new File(filepath);
+        
+        long fileSizeInBytes = file.length();
+        long fileSizeInKB = fileSizeInBytes / 1024;
+        long fileSizeInMB = fileSizeInKB / 1024;
+        
+        return fileSizeInMB<500;
+    }
+}
