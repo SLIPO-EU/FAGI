64e9482d24a2e8eae279e355cf5c1867e8c52bbd
nkaragiannakis <karagiannakis.nk@gmail.com>
2017-07-28 23:33:51 +0300

    add xml validator

---
+++ src/main/java/gr/athena/innovation/fagi/core/rule/XmlValidator.java
@@ -0,0 +1,72 @@
+package gr.athena.innovation.fagi.core.rule;
+
+import gr.athena.innovation.fagi.model.InterlinkedPair;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.*;
+import java.net.URL;
+import org.xml.sax.SAXException;
+//import java.io.File; // if you use File
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Validates the rules XML input against a preconfigured XSD
+ * 
+ * @author nkarag
+ */
+public class XmlValidator {
+    
+    private static final Logger logger = LogManager.getLogger(XmlValidator.class);
+    
+    
+    public void validate(String xmlPath, String xsdPath) throws MalformedURLException{
+        URL schemaFile = new URL(xsdPath);
+        Source xmlFile = new StreamSource(new File(xmlPath));
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        try {
+
+            Schema schema = schemaFactory.newSchema(schemaFile);
+            Validator validator = schema.newValidator();
+            validator.validate(xmlFile);
+            System.out.println(xmlFile.getSystemId() + " is valid");
+
+        } catch (SAXException e) {
+            System.out.println(xmlFile.getSystemId() + " is NOT valid reason:" + e);
+        } catch (IOException e) {
+            
+        }
+    }
+    
+    public boolean validateAgainstXSD(String xmlPath, String xsdPath) throws FileNotFoundException {
+        
+        InputStream xml = new FileInputStream(xmlPath);
+        InputStream xsd = new FileInputStream(xsdPath);
+        
+        try {
+            
+            
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(new StreamSource(xsd));
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(xml));
+            
+            logger.debug("successss");
+            return true;
+            
+        } catch (SAXException e) {
+            System.out.println("Rules in XML are NOT valid reason:" + e);
+            return false;
+        } catch (IOException e) {
+            System.out.println(e);
+            return false;
+        }  
+    }    
+}
