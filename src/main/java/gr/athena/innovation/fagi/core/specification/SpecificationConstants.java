41f07c221a15cfa66f5b5a3b9c96e1b27e992b3f
nkaragiannakis <karagiannakis.nk@gmail.com>
2017-07-28 23:37:14 +0300

    add specification constants

---
+++ src/main/java/gr/athena/innovation/fagi/core/specification/SpecificationConstants.java
@@ -0,0 +1,22 @@
+package gr.athena.innovation.fagi.core.specification;
+
+/**
+ *
+ * @author nkarag
+ */
+public class SpecificationConstants {
+
+    public static final String PROPERTY_A = "propertyA";
+    public static final String PROPERTY_B = "propertyB";
+    public static final String CONDITION = "condition";
+    public static final String ACTION_RULE_SET = "ACTION_RULE_SET";
+    public static final String ON_PROPERTY = "onProperty";
+    public static final String METHOD = "method";
+    public static final String FUNCTION = "FUNCTION";
+    public static final String EXPRESSION = "EXPRESSION";
+    public static final String AND = "AND";
+    public static final String OR = "OR";
+    public static final String NOT = "NOT";
+    
+
+}
