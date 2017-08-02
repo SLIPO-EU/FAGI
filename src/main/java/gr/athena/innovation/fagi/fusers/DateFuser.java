85fd10f980721c62f0a9920bf0602e6268692ab1
nkaragiannakis <karagiannakis.nk@gmail.com>
2017-08-02 20:20:29 +0300

    add Date and Exists fusion utils

---
+++ src/main/java/gr/athena/innovation/fagi/fusers/DateFuser.java
@@ -0,0 +1,43 @@
+package gr.athena.innovation.fagi.fusers;
+
+import gr.athena.innovation.fagi.core.specification.SpecificationConstants;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+
+/**
+ *
+ * @author nkarag
+ */
+public class DateFuser {
+    
+    private static final org.apache.logging.log4j.Logger logger = LogManager.getLogger(DateFuser.class);
+    
+    public boolean isDateKnownFormat(String date){
+
+        if (!StringUtils.isBlank(date)) {
+            for (String parse : SpecificationConstants.DATE_FORMATS) {
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat(parse);
+
+                try {
+                    Date la = simpleDateFormat.parse(date);
+                } catch (ParseException ex) {
+                    logger.error("Error parsing date format: " + date, ex);
+                }
+
+            }
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    
+    public String transformDateToFormat(String date, String format){
+        SimpleDateFormat parser = new SimpleDateFormat("EEE MMM d HH:mm:ss zzz yyyy");
+        
+        return "";
+    }
+}
